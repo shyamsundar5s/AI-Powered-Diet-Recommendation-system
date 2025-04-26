8704e3addcee1d819815c81c39b2838b1279d693
Shyamsundar Subramani <shyams0162@gmail.com>
2025-04-26 23:42:00 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI-Powered Diet Recommendation</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
